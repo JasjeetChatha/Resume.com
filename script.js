b36707b36b15f5b4489f50881ca4a040a816b8ef
Jasjeet Singh Chatha <122579616+JasjeetChatha@users.noreply.github.com>
2024-07-31 22:13:41 -0400

    added resume

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ hamburger.addEventListener('click', function () {
         menu.style.display = "block"
     }
 });
+function downloadPDF() {
+    // URL of the PDF file
+    var pdfUrl = 'https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:94e4cb73-1998-3a60-bae7-fa25f71060c1';
+    
+    // Create a temporary link element
+    var link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'Jasjeet_Resume.pdf'; // Name of the downloaded file
+    
+    // Append link to the body (not visible)
+    document.body.appendChild(link);
+    
+    // Programmatically click the link
+    link.click();
+    
+    // Remove the link from the document
+    document.body.removeChild(link);
+}
